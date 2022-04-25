5ff7a449dad9906b8da359dcf447ebc95017cc8b
maqsoom <maqsoomahmed98@gmail.com>
2022-04-24 20:55:33 -0700

    initial commit

---
+++ pages/views.py
@@ -0,0 +1,13 @@
+from re import template
+from tempfile import tempdir
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'About.html'
+# Create your views here.
